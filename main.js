4bf54ec2589c874d71d92defb7a59d0698e34d11
Likhita <keerti.likhita2@gmail.com>
2026-03-02 22:47:52 +0530

    Change fetch request to use JSON instead of form data

--- main.js
+++ main.js
@@ -229,21 +229,15 @@ async function finishProcess() {
 
     console.log("Final data being sent:", collectedData);
 
-  const formData = new URLSearchParams();
-  formData.append("state", collectedData.state);
-  formData.append("district", collectedData.district);
-  formData.append("crop", collectedData.crop);
-  formData.append("land_area", collectedData.land_area);
-  formData.append("sowing_date", collectedData.sowing_date);
-  formData.append("end_date", collectedData.end_date || "");
-  
-  const response = await fetch(ML_BACKEND_URL, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded"
-    },
-    body: formData.toString()
-  });
+const response = await fetch(ML_BACKEND_URL, {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json"
+  },
+  body: JSON.stringify(collectedData)
+});
+
+const result = await response.json();;
 
     const result = await response.json();
 
